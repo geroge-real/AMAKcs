d82afcc74b31a0c5d24d7511e9eba86887835ca4
Glitch (glitch-hello-node) <none>
2024-12-13 02:01:35 +0000

    🚇🍞 Checkpoint ./public/index.html:70502222/5366 ./public/style.css:70502222/6622 ./public/script.js:70502222/2274 ./public/aboutus.html:70502222/1719 ./public/mainprofile.html:70502222/1244 ./public/profile.html:70502222/1571 ./public/setting.html:70502222/80

---
+++ public/script.js
@@ -0,0 +1,84 @@
+//function for searching
+function searchBooks() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const books = document.getElementsByClassName('book');
+    
+    for (let i = 0; i < books.length; i++) {
+        const title = books[i].getAttribute('data-title').toLowerCase();
+        const author = books[i].getAttribute('data-author').toLowerCase();
+        
+        if (title.includes(searchInput) || author.includes(searchInput)) {
+            books[i].style.display = "";
+        } else {
+            books[i].style.display = "none";
+        }
+    }
+}
+
+//menu button code
+
+document.querySelector('.menu-button').addEventListener('click', function() {
+  const menuContent = document.querySelector('.menu-content');
+  menuContent.classList.toggle('show');
+});
+
+window.addEventListener('click', function(event) {
+  // Close the dropdown if the user clicks outside the menu
+  if (!event.target.closest('.menu')) {
+    const menuContent = document.querySelector('.menu-content');
+    if (menuContent.classList.contains('show')) {
+      menuContent.classList.remove('show');
+    }
+  }
+});
+
+
+
+//profile js to save
+window.onload = function() {
+
+    const username = localStorage.getItem('username');
+
+    const email = localStorage.getItem('email');
+
+    const books = JSON.parse(localStorage.getItem('favoriteBooks'));
+
+    if (username) document.getElementById('username').value = username;
+
+    if (email) document.getElementById('email').value = email;
+
+    if (books) {
+
+        const bookInputs = document.querySelectorAll('.book');
+
+        books.forEach((book, index) => {
+
+            if (bookInputs[index]) bookInputs[index].value = book;
+
+        });
+
+    }
+
+};
+
+// this is function to save profile to localstorage
+
+function saveProfile() {
+
+    const username = document.getElementById('username').value;
+
+    const email = document.getElementById('email').value;
+
+    const bookInputs = document.querySelectorAll('.book');
+
+    const favoriteBooks = Array.from(bookInputs).map(input => input.value);
+
+    localStorage.setItem('username', username);
+
+    localStorage.setItem('email', email);
+
+    localStorage.setItem('favoriteBooks', JSON.stringify(favoriteBooks));
+
+    alert("Profile information saved successfully!");
+
+}
